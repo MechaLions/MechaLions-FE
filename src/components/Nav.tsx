d2dc3ad972aa16ed95da0207f29d6ce8bb47c581
SonSuBin129 <ttokky0129@gmail.com>
2024-06-02 04:59:42 +0900

    feat: 현재 위치 확인해서 Navbar 텍스트 스타일

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,25 +1,49 @@
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 import Logo from "./Logo";
 import UserIcon from "./UserIcon";
-import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const location = useLocation();
+
   return (
     <nav className="shadow-md flex font-bold gap-[121px] justify-between px-[87px] py-[10px] items-center">
       <Logo />
-      <Link to="/" className="hover:text-[#0169F4] text-[16px]">
+      <NavLink to="/" currentPath={location.pathname}>
         수어 번역
-      </Link>
-      <Link to="/" className="hover:text-[#0169F4] text-[16px]">
+      </NavLink>
+      <NavLink to="/shadowing" currentPath={location.pathname}>
         수어 쉐도잉
-      </Link>
-      <Link to="/" className="hover:text-[#0169F4] text-[16px]">
+      </NavLink>
+      <NavLink to="/quiz" currentPath={location.pathname}>
         수어 퀴즈
-      </Link>
-      <Link to="/" className="hover:text-[#0169F4] text-[16px]">
+      </NavLink>
+      <NavLink to="/pronunciation" currentPath={location.pathname}>
         발음 교정
-      </Link>
+      </NavLink>
       <UserIcon />
     </nav>
+  );
+};
+
+interface NavLinkProps {
+  to: string;
+  currentPath: string;
+  children: React.ReactNode;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ to, currentPath, children }) => {
+  const isActive = currentPath === to;
+
+  return (
+    <Link
+      to={to}
+      className={`hover:text-[#0169F4] text-[16px] ${
+        isActive ? "text-[#0169F4]" : ""
+      }`}
+    >
+      {children}
+    </Link>
   );
 };
 
